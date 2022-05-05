4a5ef24b4a62a8516c04ff53d99fff00a1af5d04
eiki tsutou <eikitsutou0920@gmail.com>
2022-05-05 07:10:15 +0000

    change file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    if logged_in?
-      @task = current_user.tasks.build
-      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-    end
+    
+    @task = current_user.tasks.build
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    
   end
 
   def show
